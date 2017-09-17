bff18f9284f37befcbbe585640a56997b4045a5b
kkkacey <31811824+kkkacey@users.noreply.github.com>
2017-09-17 12:33:23 -0400

    Update bike.sh
    deleted the echo line

--- bike.sh
+++ bike.sh
@@ -5,7 +5,6 @@ wget "http://witestlab.poly.edu/bikes/20160$i-citibike-tripdata.zip"
 unzip  "20160$i-citibike-tripdata.zip"
 awk -F "\"*,\"*" '{print $5}' "20160$i-citibike-tripdata.csv" > stations.csv
 awk -F "\"*,\"*" '{print $9}' "20160$i-citibike-tripdata.csv" >> stations.csv
-echo "month $i counting completed"
 done
 
 sort stations.csv | uniq -c | sort -n -r > result.csv 
